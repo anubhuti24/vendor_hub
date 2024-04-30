b854364081345ffe00714bbc0e8758d9098ccb6f
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-04-30 23:09:27 +0530

    feature: added models

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from vendor.models import VendorProfile, PurchaseOrderModel, HistoricalModel
 
-# Register your models here.
+
+@admin.register(VendorProfile)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ["vendor_code", "name", "contact_details", "address", "on_time_delivery_rate", "quality_rating_avg", "average_response_time", "fulfillment_rate"]
+
+
+@admin.register(PurchaseOrderModel)
+class PurchaseOrderAdmin(admin.ModelAdmin):
+    list_display = ["po_number", "vendor", "order_date", "delivery_date", "items", "quantity", "status", "quality_rating", "issue_date", "acknowledgment_date"]
+
+
+@admin.register(HistoricalModel)
+class HistoricDataAdmin(admin.ModelAdmin):
+    list_display = ["vendor", "date", "on_time_delivery_rate", "quality_rating_avg", "average_response_time", "fulfillment_rate"]
--- vendor/models.py
+++ vendor/models.py
@@ -1,12 +1,48 @@
 from django.db import models
-from django.core.validators import RegexValidator
 
 
 class VendorProfile(models.Model):
     name = models.CharField(max_length=20)
-    phone_regex = models.TextField()
+    contact_details = models.TextField()
     address = models.TextField()
-    vendor_code = models.CharField(unique=True, primary_key=True)
+    vendor_code = models.CharField(max_length=50, unique=True, primary_key=True)
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} {self.vendor_code}"
+
+
+class PurchaseOrderModel(models.Model):
+    PENDING = "PEN"
+    COMPLETED = "COM"
+    CANCELLED = "CAN"
+    OrderStatus = {
+        PENDING: "PENDING",
+        COMPLETED: "COMPLETED",
+        CANCELLED: "CANCELLED"
+    }
+
+    po_number = models.CharField(max_length=50, unique=True, primary_key=True)
+    vendor = models.ForeignKey(VendorProfile, on_delete=models.CASCADE, related_name="purchase_order")
+    order_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=10, choices=OrderStatus, default=PENDING)
+    quality_rating = models.FloatField()
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.po_number}"
+
+
+class HistoricalModel(models.Model):
+    vendor = models.ForeignKey(VendorProfile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
     on_time_delivery_rate = models.FloatField()
     quality_rating_avg = models.FloatField()
     average_response_time = models.FloatField()

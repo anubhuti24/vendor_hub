bb2392034a026f78a1ce46edf20f49739d78d427
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-05-04 23:42:16 +0530

    refctor: updated purchase order serialiser

--- purchase_order/serializers.py
+++ purchase_order/serializers.py
@@ -6,4 +6,28 @@ from purchase_order.models import PurchaseOrderModel
 class PurchaseOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = PurchaseOrderModel
-        fields = "__all__"
+        fields = [
+            'po_number',
+            'vendor',
+            'order_date',
+            'delivery_date',
+            'items',
+            'quantity',
+            'status',
+            'quality_rating',
+            'issue_date',
+            'acknowledgment_date',
+        ]
+
+    def update(self, instance, validated_data):
+        instance.vendor = validated_data.get('vendor', instance.vendor)
+        instance.order_date = validated_data.get('order_date', instance.order_date)
+        instance.delivery_date = validated_data.get('delivery_date', instance.delivery_date)
+        instance.items = validated_data.get('items', instance.items)
+        instance.quantity = validated_data.get('quantity', instance.quantity)
+        instance.status = validated_data.get('status', instance.status)
+        instance.quality_rating = validated_data.get('quality_rating', instance.quality_rating)
+        instance.issue_date = validated_data.get('issue_date', instance.issue_date)
+        instance.acknowledgment_date = validated_data.get('acknowledgment_date', instance.acknowledgment_date)
+        instance.save()
+        return instance

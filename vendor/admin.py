78817357c273b301f1a357a357054abebc4b0c24
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-05-05 12:24:02 +0530

    refactor: admin response

--- vendor/admin.py
+++ vendor/admin.py
@@ -5,7 +5,12 @@ from vendor.models import VendorProfile, HistoricalModel
 @admin.register(VendorProfile)
 class VendorAdmin(admin.ModelAdmin):
     list_display = ["vendor_code", "name", "contact_details", "address",
-                    "on_time_delivery_rate", "quality_rating_avg", "average_response_time", "fulfillment_rate"]
+                    "on_time_delivery_rate", "quality_rating_avg", "average_response_time_in_hours", "fulfillment_rate"]
+
+    def average_response_time_in_hours(self, obj):
+        return f"{obj.average_response_time:.2f} hours"
+
+    average_response_time_in_hours.short_description = "Average Response Time (in hours)"
 
 
 @admin.register(HistoricalModel)

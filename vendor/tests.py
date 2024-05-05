5fb7f865192210594f402f89e7ab2b32758cfca9
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-05-05 17:48:49 +0530

    feature: added test suite for vendor

--- vendor/tests.py
+++ vendor/tests.py
@@ -1,3 +1,72 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+class VendorAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='aaa', password='11')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token}')
+
+    def test_vendors_endpoints(self):
+        # Test POST request to create a new vendor
+        response = self.client.post('/api/vendors/', {
+              "name": "James Willie",
+              "contact_details": "7677778788",
+              "address": "6/12 Robert House",
+              "vendor_code": "VENDOR001",
+              "on_time_delivery_rate": 0.95,
+              "quality_rating_avg": 4.5,
+              "average_response_time": 2.5,
+              "fulfillment_rate": 0.98
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Test GET request to retrieve all vendors
+        response = self.client.get('/api/vendors/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Test PUT request to update a vendor
+        response = self.client.put(f'/api/vendors/VENDOR001/', {
+            "name": "XYZ",
+            "contact_details": "9897806985",
+            "address": "6/12 Robert House",
+            "vendor_code": "VENDOR001",
+            "on_time_delivery_rate": 0.95,
+            "quality_rating_avg": 4.5,
+            "average_response_time": 2.5,
+            "fulfillment_rate": 0.98
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Test GET request to retrieve performance metrics of a vendor
+        response = self.client.get(f'/api/vendors/VENDOR001/performance/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Test DELETE request to delete a vendor
+        response = self.client.delete(f'/api/vendors/VENDOR001/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_nonexistent_vendor_fetch_update_delete(self):
+        # Test handling of nonexistent vendor
+        response = self.client.get('/api/vendors/VENDOR092/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        response = self.client.put('/api/vendors/VENDOR092/', {
+            "name": "Alice Walker",
+            "contact_details": "9897806985",
+            "address": "",
+            "vendor_code": "VENDOR001",
+            "on_time_delivery_rate": 0.95,
+            "quality_rating_avg": 4.5,
+            "average_response_time": 2.5,
+            "fulfillment_rate": 0.98
+        })
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        response = self.client.delete('/api/vendors/VENDOR092/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
